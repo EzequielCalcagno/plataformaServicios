1c6ca59e24da4eff2fae3b4c7b3369ec54aba564
EzequielCalcagno <ezequielcalcagno@gmail.com>
2025-10-27 23:56:57 -0300

    Ajustes en el backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-// src/server.ts
+// src/app.ts
 import express from 'express';
 // import publicRouter from './routes/public.route';
 // import authRouter from './routes/auth.route';
@@ -8,14 +8,17 @@ import pingRouter from './routes/ping.route';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 
 // Routes
 // app.use('/', publicRouter);
 // app.use('/v1/auth', authRouter);
 // app.use('/v1', privateRouter);
-app.use('/v1/ping', pingRouter);
+app.use('/api/v1', pingRouter);
 
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
+
+export default app;
